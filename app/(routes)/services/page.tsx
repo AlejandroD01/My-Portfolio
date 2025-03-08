f59b697a254fbdc55800baf854133e4b807cc2ea
Alejandro David <alejandrodavidmonagas@gmail.com>
2025-03-07 23:31:27 -0500

    Update ServicesPage content and enhance Swiper component styling for improved user experience

--- app/(routes)/services/page.tsx
+++ app/(routes)/services/page.tsx
@@ -16,7 +16,7 @@ const ServicesPage = () => {
                 <div className=" ml-8 mr-8 max-w-[450px] mt-20 md:mt-0" >
 
                     <h1 className="text-2xl leading-tight text-center md:text-center md:text-4xl md:mb-5">Mis <span className="font-bold text-secondary"> servicios.</span></h1>
-                    <p className="mb-3  text-xl  text-gray-300">Ofrezco servicios de desarrollo web Full Stack y especializados en la creación de sitios web y aplicaciones atractivas y funcionales. Utilizando las últimas tecnologías, como HTML, CSS y JavaScript, diseño interfaces de usuario intuitivas y responsivas que reflejan la identidad de marca de mis clientes y mejoran su presencia en línea.</p>
+                    <p className="mb-3  text-xl  text-gray-300">Ofrezco servicios especializados en desarrollo web. Empleando tecnologías de vanguardia como React.js, Java y WordPress, me dedico a crear interfaces de usuario altamente intuitivas, responsivas y adaptadas a las necesidades específicas de cada cliente.</p>
                     <ButtonContactaConmigo/>
                 </div>
 
--- components/slider-services.tsx
+++ components/slider-services.tsx
@@ -7,41 +7,64 @@ import { serviceData } from '@/data';
 
 const SliderServices = () => {
     return (
-        <Swiper
-            breakpoints={{
-                320: {
-                    slidesPerView: 1,
-                    spaceBetween: 15
-                },
-                768: {
-                    slidesPerView: 2,
-                    spaceBetween: 15
-                },
-                1024: {
-                    slidesPerView: 3,
-                    spaceBetween: 15
-                }
-            }}
-            freeMode={true}
-            pagination={{
-                clickable: true
-            }}
-            modules={[Pagination]}
-            className="h-[280px] md:h-[340px] w-[270px] md:w-[550px]"
-        >
+        <>
+            <Swiper
+                breakpoints={{
+                    320: {
+                        slidesPerView: 1,
+                        spaceBetween: 15
+                    },
+                    768: {
+                        slidesPerView: 2,
+                        spaceBetween: 15
+                    },
+                    1024: {
+                        slidesPerView: 3,
+                        spaceBetween: 15
+                    }
+                }}
+                freeMode={true}
+                pagination={{
+                    clickable: true
+                }}
+                modules={[Pagination]}
+                className="h-[280px] md:h-[340px] w-[270px] md:w-[550px]"
+            >
 
-            {serviceData.map((item, index) => (
-                <SwiperSlide key={index} >
-                    <div className="flex px-6 py-8 h-auto md:h-[290px] rounded-lg cursor-pointer bg-[rgba(65,47,123,0.15)] sm:flex-col gap-x-6 sm:gap-x-0 group hover:bg-[rgba(89,65,169,0.15)] transition-all duration-300 hover:border-secondary border-2">
-                        <div className="mb-4 text-4xl text-secondary">{item.icon}</div>
-                        <div>
-                            <h3 className="mb-4 text-lg">{item.title}</h3>
-                            <p className="text-sm">{item.description}</p>
+                {serviceData.map((item, index) => (
+                    <SwiperSlide key={index} >
+                        <div className="flex px-6 py-8 h-auto md:h-[290px] rounded-lg cursor-pointer bg-[rgba(65,47,123,0.15)] sm:flex-col gap-x-6 sm:gap-x-0 group hover:bg-[rgba(89,65,169,0.15)] transition-all duration-300 hover:border-secondary border-2">
+                            <div className="mb-4 text-4xl text-secondary">{item.icon}</div>
+                            <div>
+                                <h3 className="mb-4 text-lg">{item.title}</h3>
+                                <p className="text-sm">{item.description}</p>
+                            </div>
                         </div>
-                    </div>
-                </SwiperSlide>
-            ))}
-        </Swiper>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+
+            <style jsx global>{`
+             @media (max-width: 768px) { 
+                .swiper-pagination {
+                    position: absolute;
+                    bottom: 45px !important; /* Ajusta esta valor para subir/bajar */
+                    margin-top: 15px;
+                }
+            }
+                .swiper-pagination-bullet {
+                    background: #8B5CF6 !important;
+                    opacity: 0.5;
+                    width: 8px;
+                    height: 8px;
+                }
+
+                .swiper-pagination-bullet-active {
+                    opacity: 1;
+                    background: #7C3AED !important;
+                }
+            `}</style>
+        </>
     );
 }
 
